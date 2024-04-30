1c233719996b9f732c16ddac30a3faea42a66244
franmacke <fmacke10@gmail.com>
2024-04-30 00:59:21 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,10 @@
 from yt_dlp import YoutubeDL
 import sys
 
-YTDL_OPTIONS = {
+YTDL_OPTIONS_AUDIO = {
     "format": "bestaudio/best",
     "extractaudio": True,
-    "audioformat": "mp3",
-    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "outtmpl": "%(title)s.%(ext)s",
     "restrictfilenames": True,
     "noplaylist": False,
     "nocheckcertificate": True,
@@ -22,8 +21,22 @@ YTDL_OPTIONS = {
     }],
 }
 
-def download_song(song):
-    downloader = YoutubeDL(YTDL_OPTIONS)
+YTDL_OPTIONS_VIDEO = {
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+    "outtmpl": "%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": False,
+    "nocheckcertificate": True,
+    "ignoreerrors": True,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": False,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",
+}
+
+def download_media(song, options):
+    downloader = YoutubeDL(options)
     info = downloader.extract_info(song, download=True)
     
     title = info.get("title")
@@ -41,4 +54,10 @@ def download_song(song):
     return title, url, duration, thumbnail, website_url
 
 if __name__ == "__main__":
-    download_song(sys.argv[1])
+    song_url = sys.argv[1]
+    download_audio = sys.argv[2].lower() == 'audio'
+    
+    if download_audio:
+        download_media(song_url, YTDL_OPTIONS_AUDIO)
+    else:
+        download_media(song_url, YTDL_OPTIONS_VIDEO)
